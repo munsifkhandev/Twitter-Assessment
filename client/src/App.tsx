4285d5c93c275f3792e7fb77411f5cd197b0b0c9
munsifkhandev <munsifkhandev@gmail.com>
2026-01-09 11:52:14 +0500

    Initialized the Frontend Code

---
+++ client/src/App.tsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <>
+      <Toaster />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
